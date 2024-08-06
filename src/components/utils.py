2d7c4886659575db28bd74c351c08dae8b4ab75c
TP <thomas.philip@vectorglobe.com>
2024-08-06 16:57:59 +0530

    hypertuning parameter

--- src/components/utils.py
+++ src/components/utils.py
@@ -6,6 +6,7 @@ from logger import logging
 from exception import CustomException
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from exception import CustomException
 
@@ -21,18 +22,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(x_train, y_train,x_test,y_test,models):
+def evaluate_models(x_train, y_train,x_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
 
-           # para=param[list(models.keys())[i]]
-           # gs = GridSearchCV(model,para,cv=3)
-           #gs.fit(x_train,y_train)
+            param=params[list(models.keys())[i]]
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(x_train,y_train)
 
-            #model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
