e4b09c438caebef2721b2cdae63747276274d52b
TP <thomas.philip@vectorglobe.com>
2024-08-06 16:38:08 +0530

    model_trainer done

---
+++ src/components/model_trainer.py
@@ -0,0 +1,94 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+
+from exception import CustomException
+from logger import logging
+from utils import save_object,evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifact","model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Split training and test input data")
+            x_train,y_train,x_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+        
+
+            model_report:dict=evaluate_models(x_train=x_train,
+                                              y_train=y_train,
+                                              x_test=x_test,
+                                              y_test=y_test,
+                                              models=models)
+            
+
+            # TO GET BEST MODEL SCORE FROM DICT
+            best_model_score = max(sorted(model_report.values()))
+
+
+            # TO GET BEST MODEL NAME
+
+            best_model_name = list(model_report.keys())[
+
+            list (model_report.values()).index(best_model_score)
+
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            
+          
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(x_test)
+
+            r2_square = r2_score(y_test, predicted)
+
+            logging.info(f" Best found model on both training and testing dataset is {best_model} with r2_score of {r2_square}")
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
--- src/components/utils.py
+++ src/components/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from logger import logging
 from exception import CustomException
 import pickle
+from sklearn.metrics import r2_score
 
 from exception import CustomException
 
@@ -20,3 +21,40 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
+def evaluate_models(x_train, y_train,x_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+           # para=param[list(models.keys())[i]]
+           # gs = GridSearchCV(model,para,cv=3)
+           #gs.fit(x_train,y_train)
+
+            #model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
