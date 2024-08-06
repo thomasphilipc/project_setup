687c6033887da0433821509634bdc8f4f74532ec
TP <thomas.philip@vectorglobe.com>
2024-08-06 13:00:14 +0530

    data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,61 @@
+## The component is responsible to get data from sources
+
+import os
+import sys
+# below two lines were added to solve the src not found issue
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
+
+
+from logger import logging
+from exception import CustomException
+import pandas as pd
+
+
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+#any input required is handled by the DataIngestionConfig , extend this in future to support api_data, folder_data etc
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifact','train.csv')
+    test_data_path: str=os.path.join('artifact','test.csv')
+    raw_data_path: str=os.path.join('artifact','raw.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    #currently it expects the data to be present in  a particular file called stud.csv
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Imported the csv data file into dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train and Test split initiated")
+
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info('Ingestion completed and train, test files saved')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -17,6 +17,3 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-
-if __name__=="__main__":
-    logging.info("Logging has started")
